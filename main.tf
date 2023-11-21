1c5732523324bff5a79d33b58573fe1d430eb891
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-21 10:56:46 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -67,4 +67,12 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
+resource "aws_route53_record" "main" {
+  zone_id = var.zone_id
+  name    = "${var.component}-${var.env}"
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.alb_name]
+}
+
 
