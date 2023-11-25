b7a65ff174a29756ebc862f328808f93a68c5a4f
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-25 16:12:03 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -100,8 +100,8 @@ resource "aws_lb_listener_rule" "main" {
 
 
 resource "aws_lb_target_group" "public" {
-  count = var.component == frontend ? 1 : 0
-  name     = "${local.name_prefix}-tg"
+  count = var.component == "frontend" ? 1 : 0
+  name     = "${local.name_prefix}-public"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
